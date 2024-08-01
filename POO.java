f57b69c0516e9fe03c8cbfe2fa3d7e832035dd2e
Braulio Hernández <72417322+BraulioHzGz@users.noreply.github.com>
2024-08-01 11:30:42 -0600

    Create POO.java

---
+++ POO.java
@@ -0,0 +1,35 @@
+package MAIN_PACKAGE;
+
+/**
+ *
+ * @author Brau_
+ */
+public class POO {
+    public static void main(String args[]){
+        
+        Ordenamientos ord = new Ordenamientos();
+        Busquedas search;
+        
+        float array[] = 
+            {5.6f, 7.4f, 1.2f, 0.6f, -4.5f, 0.9f, -3.5f, 4.4f, -20.5f};
+        
+        int a[] = {-5, 6, 1, 0, -6, 20, -35, 17, -14, -2};
+        
+        //search = new Busquedas("Lineal", array);
+        
+        //ord.BubbleSort(array);
+        //ord.CocktailSort(array); 
+        //ord.CombSort(array);
+        //ord.CountingSort(a); 
+        //ord.HeapSort(array);
+        //ord.InsertionSort(array);
+        //ord.SelectionSort(array);
+        //ord.ShellSort(array);
+        
+        //ord.MergeSort(array, 0, array.length - 1);
+        //ord.QuickSort(array, 0, array.length - 1);
+        //ord.tools.PrintArray(array, array.length);
+        
+        //search = new Busquedas("Binaria", array);
+    }
+}
